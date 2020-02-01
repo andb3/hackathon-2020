9b86042153c317eec72b53bbe11472b571d0a3a9
andb3 <andb3003@yahoo.com>
2020-02-01 11:59:28 -0600

    added region.js

---
+++ regions.js
@@ -0,0 +1,13 @@
+class Region{
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+    }
+}
+
+var allRegions = [
+    new Region(1, "New York, NY"),
+    new Region(2, "Washington, DC"),
+    new Region(3, "Chicago, IL"),
+    new Region(4, "Los Angeles, CA")
+];
