d412765d338be326cacf4d71415eeedcca933a8f
AwokenPhenix <just4porchia@gmail.com>
2020-02-01 14:19:04 -0500

    Event.js

--- js/Event.js
+++ js/Event.js
@@ -10,7 +10,6 @@ class Event {
 }
 
 var allEvents = [
-
     new Event = (1, "Trash Clean up", "Central Park", 1),
     new Event = (2, "Recycling Workshop", "Empire Sate Building", 1),
     new Event = (3, "Tree Planting", "Time Square", 1),
@@ -20,5 +19,11 @@ var allEvents = [
     new Event = (7, "Tree Planting", "White house", 2),
     new Event = (8, "Recycling Work", "H St", 2),
 
-
 ]
+var threads = getEventsFor(1);
+events.forEach(events => {
+    node = document.createElement("li");
+    node.innerHTML = events.name;
+    document.getElementById("event_holder").appendChild(node);
+}
+)
