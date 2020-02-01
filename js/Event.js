8fe0efa17b88ddce62beb535ca3aa410b71771b9
AwokenPhenix <just4porchia@gmail.com>
2020-02-01 14:36:42 -0500

    Link events with HTML

--- js/Event.js
+++ js/Event.js
@@ -10,20 +10,24 @@ class Event {
 }
 
 var allEvents = [
-    new Event = (1, "Trash Clean up", "Central Park", 1),
-    new Event = (2, "Recycling Workshop", "Empire Sate Building", 1),
-    new Event = (3, "Tree Planting", "Time Square", 1),
-    new Event = (4, "Pollution protest", "Statue of Liberty", 1),
-    new Event = (5, "Landfill Clean up","Capitol Building", 2),
-    new Event = (6, "Vegan Seminar", "Washington Monument", 2),
-    new Event = (7, "Tree Planting", "White house", 2),
-    new Event = (8, "Recycling Work", "H St", 2),
+    new Event  (1, "Trash Clean up", "Central Park", 1),
+    new Event  (2, "Recycling Workshop", "Empire Sate Building", 1),
+    new Event  (3, "Tree Planting", "Time Square", 1),
+    new Event  (4, "Pollution protest", "Statue of Liberty", 1),
+    new Event  (5, "Landfill Clean up","Capitol Building", 2),
+    new Event  (6, "Vegan Seminar", "Washington Monument", 2),
+    new Event  (7, "Tree Planting", "White house", 2),
+    new Event  (8, "Recycling Work", "H St", 2),
 
 ]
-var threads = getEventsFor(1);
-events.forEach(events => {
+
+function getEventsFor(regionID){
+    return allEvents.filter(event => event.regionID == regionID);
+}
+var events = getEventsFor(1);
+events.forEach(event => {
     node = document.createElement("li");
-    node.innerHTML = events.name;
+    node.innerHTML = event.name;
     document.getElementById("event_holder").appendChild(node);
 }
 )
